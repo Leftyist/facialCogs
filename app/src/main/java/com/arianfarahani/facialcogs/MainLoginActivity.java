2da7187dcac928f055ba9cfc740ed08e65276c0b
Leftyist <arianfarahani@gmail.com>
2017-11-11 21:48:14 -0500

    Front Facing camera preview

--- app/src/main/java/com/arianfarahani/facialcogs/MainLoginActivity.java
+++ app/src/main/java/com/arianfarahani/facialcogs/MainLoginActivity.java
@@ -1,402 +1,203 @@
 package com.arianfarahani.facialcogs;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
  * A login screen that offers login via email/password.
  */
-public class MainLoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>
+public class MainLoginActivity extends AppCompatActivity
 {
+    private static final String TAG                = "MainActivity";
+    final                int    REQUEST_USE_CAMERA = 0;
+    SurfaceView surfaceView;
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
+    CameraDevice.StateCallback cameraInterface = new CameraDevice.StateCallback()
+    {
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice)
+        {
+            surfaceView     = (SurfaceView) findViewById(R.id.surfaceView);
+            SurfaceHolder holder = surfaceView.getHolder();
+            List<Surface> list   = new ArrayList<Surface>();
+            list.add(holder.getSurface());
+            try
+            {
+                cameraDevice.createCaptureSession(list, captureInterface, null);
+            } catch (Exception e)
+            {
+                Log.d(TAG, e.toString());
+                Toast.makeText(MainLoginActivity.this,
+                               "Couldn't capture session.",
+                               Toast.LENGTH_SHORT
+                )
+                     .show();
+            }
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[]      DUMMY_CREDENTIALS = new String[]{"foo@example.com:hello",
-                                                                        "bar@example.com:world"
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice)
+        {
+            Toast.makeText(MainLoginActivity.this, "Camera Disconnected.", Toast.LENGTH_SHORT)
+                 .show();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i)
+        {
+            Toast.makeText(MainLoginActivity.this, "Camera Error.", Toast.LENGTH_SHORT).show();
+        }
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private              UserLoginTask mAuthTask         = null;
 
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText             mPasswordView;
-    private View                 mProgressView;
-    private View                 mLoginFormView;
+    CameraCaptureSession.StateCallback captureInterface = new CameraCaptureSession.StateCallback()
+    {
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession)
+        {
+            Log.e(TAG, "Config Failed");
+        }
+
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession)
+        {
+            Log.d(TAG, "Config Success");
+            try
+            {
+                CaptureRequest.Builder builder = cameraCaptureSession.getDevice()
+                                                        .createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+                builder.addTarget(surfaceView.getHolder().getSurface());
+
+                cameraCaptureSession.setRepeatingRequest(builder.build(), captureCallback, null);
+            } catch (Exception e)
+            {
+                Log.e(TAG, e.toString());
+            }
+
+        }
+    };
+
+    CameraCaptureSession.CaptureCallback captureCallback
+            = new CameraCaptureSession.CaptureCallback()
+    {
+
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent)
-            {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL)
-                {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener()
+        Button capButton = (Button) findViewById(R.id.button_capture);
+        capButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                attemptLogin();
+                checkPermissions();
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
     }
 
-    private void populateAutoComplete()
+    private void checkPermissions()
     {
-        if (!mayRequestContacts())
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED)
         {
-            return;
-        }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts()
-    {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-        {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED)
-        {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS))
-        {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener()
-                    {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v)
-                        {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.CAMERA},
+                                              REQUEST_USE_CAMERA
+            );
         } else
         {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+            getCameraInstance();
         }
-        return false;
     }
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
     @Override
     public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+            int requestCode, String permissions[], int[] grantResults)
     {
-        if (requestCode == REQUEST_READ_CONTACTS)
+        switch (requestCode)
         {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            case REQUEST_USE_CAMERA:
             {
-                populateAutoComplete();
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    getCameraInstance();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else
+                {
+                    Toast.makeText(this, "Permissions not granted :(", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
             }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
-    }
-
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    private void attemptLogin()
-    {
-        if (mAuthTask != null)
-        {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email    = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel    = false;
-        View    focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password))
-        {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email))
-        {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email))
-        {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel)
-        {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else
-        {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
-        }
-    }
-
-    private boolean isEmailValid(String email)
-    {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password)
-    {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
     }
 
     /**
-     * Shows the progress UI and hides the login form.
+     * A safe way to get an instance of the Camera object.
      */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show)
+    public void getCameraInstance()
     {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
-        {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        CameraManager cameraManager = this.getSystemService(CameraManager.class);
 
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter()
+        try
+        {
+            String[] cameras = cameraManager.getCameraIdList();
+            for (String camera : cameras)
             {
-                @Override
-                public void onAnimationEnd(Animator animation)
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(
+                        camera);
+                if (characteristics.get(CameraCharacteristics.LENS_FACING)
+                        == CameraCharacteristics.LENS_FACING_FRONT)
                 {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter()
-            {
-                @Override
-                public void onAnimationEnd(Animator animation)
-                {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else
-        {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
-    {
-        return new CursorLoader(this,
-                                // Retrieve data rows for the device user's 'profile' contact.
-                                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                                                     ContactsContract.Contacts.Data.CONTENT_DIRECTORY
-                                ),
-                                ProfileQuery.PROJECTION,
-
-                                // Select only email addresses.
-                                ContactsContract.Contacts.Data.MIMETYPE + " = ?",
-                                new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE
-                                },
-
-                                // Show primary email addresses first. Note that there won't be
-                                // a primary email address if the user hasn't specified one.
-                                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC"
-        );
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
-    {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast())
-        {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader)
-    {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection)
-    {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainLoginActivity.this,
-                                                          android.R.layout.simple_dropdown_item_1line,
-                                                          emailAddressCollection
-        );
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery
-    {
-        String[] PROJECTION = {ContactsContract.CommonDataKinds.Email.ADDRESS, ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-                               };
-
-        int ADDRESS    = 0;
-        int IS_PRIMARY = 1;
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean>
-    {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password)
-        {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params)
-        {
-            // TODO: attempt authentication against a network service.
-
-            try
-            {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e)
-            {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS)
-            {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail))
-                {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                    cameraManager.openCamera(camera, cameraInterface, null);
                 }
             }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success)
+            // attempt to get a Camera instance
+        } catch (SecurityException e)
         {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success)
-            {
-                finish();
-            } else
-            {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled()
+            Toast.makeText(this, "No camera found :(", Toast.LENGTH_SHORT).show();
+        } catch (Exception ex)
         {
-            mAuthTask = null;
-            showProgress(false);
+            Toast.makeText(this, "No camera found :(", Toast.LENGTH_SHORT).show();
         }
+        return; // returns null if camera is unavailable
     }
 }
 
